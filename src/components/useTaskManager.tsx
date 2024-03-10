8ae9924c8addeb12a80636e7133d83fb688efd16
Antenaina2002 <hei.antenaina@gmail.com>
2024-03-11 00:44:46 +0300

    feat: useTaskManager hook integration

--- src/components/useTaskManager.tsx
+++ src/components/useTaskManager.tsx
@@ -18,41 +18,41 @@ interface TaskManagerOperations {
 }
 
 export const useTaskManager = (): TaskManagerOperations => {
-  const [title, setTitle] = useState<string>("");
-  const [searchKeyword, setSearchKeyword] = useState<string>("");
-  const [tasks, setTasks] = useState<Task[]>([]);
+    const [title, setTitle] = useState<string>("");
+    const [searchKeyword, setSearchKeyword] = useState<string>("");
+    const [tasks, setTasks] = useState<Task[]>([]);
 
-  const completeTask = (id: string) => {
-    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
-  };
-
-  const updateTask = (id: string, taskUpdate: Partial<Task>) => {
-    setTasks((prevTasks) =>
-      prevTasks.map((task) => (task.id === id ? { ...task, ...taskUpdate } : task))
-    );
-  };
-
-  const addTask = () => {
-    if (title.length < 1) {
-      return;
-    }
-
-    const newTask: Task = {
-      id: nanoid(),
-      title,
+    const completeTask = (id: string) => {
+        setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
     };
 
-    setTasks((prevTasks) => [...prevTasks, newTask]);
-    setTitle("");
-  };
+    const updateTask = (id: string, taskUpdate: Partial<Task>) => {
+        setTasks((prevTasks) =>
+        prevTasks.map((task) => (task.id === id ? { ...task, ...taskUpdate } : task))
+        );
+    };
 
-  const handleSearch = (searchKeyword: string) => {
-    setSearchKeyword(searchKeyword);
-  };
+    const addTask = () => {
+        if (title.length < 1) {
+        return;
+        }
 
-  const filteredTasks = tasks.filter((task) =>
-    task.title.toLowerCase().includes(searchKeyword.toLowerCase())
-  );
+        const newTask: Task = {
+        id: nanoid(),
+        title,
+        };
+
+        setTasks((prevTasks) => [...prevTasks, newTask]);
+        setTitle("");
+    };
+
+    const handleSearch = (searchKeyword: string) => {
+        setSearchKeyword(searchKeyword);
+    };
+
+    const filteredTasks = tasks.filter((task) =>
+        task.title.toLowerCase().includes(searchKeyword.toLowerCase())
+    );
 
     return {
         addTask,
